f614b282c3c95366f3dfb6fa0dc74f72de10dc3a
Atheek <Atheekmohamad@gmail.com>
2022-02-26 00:30:43 +0530

    Mrzdeals

--- src/app/Member-Directory/service-details/service-details.component.ts
+++ src/app/Member-Directory/service-details/service-details.component.ts
@@ -10,6 +10,7 @@ export class ServiceDetailsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ServiceDetailsComponent } from './Member-Directory/service-details/service-details.component';
 import { ServiceCardComponent } from './Member-Directory/service-card/service-card.component';
 import { CreateprofileComponent } from './Member-Directory/createprofile/createprofile.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
@@ -7,15 +8,18 @@ import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'login',component:AuthenticationComponent},
   {path:'services',component:ServiceListComponent},
+  {path:'service',component:ServiceDetailsComponent},
   {path:'messages',component:MessagesComponent},
   {path:'card',component:ServiceCardComponent},
   {path:'createprofile',component:CreateprofileComponent},
   {path:'services',component:ServiceListComponent},
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
